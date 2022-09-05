e4dd94e92e3b820b82cc714c83befc1316aa66a1
Marcos Irazabal <marcos.irazabal@accenture.com>
2022-09-05 11:24:39 -0300

    agregando rutas hijas

---
+++ src/app/recetas/receta-detalle/receta-detalle.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receta-detalle',
+  templateUrl: './receta-detalle.component.html',
+  styleUrls: ['./receta-detalle.component.css']
+})
+export class RecetaDetalleComponent  {
+
+  constructor() { }
+
+  show(){
+    return true
+  }
+
+}
